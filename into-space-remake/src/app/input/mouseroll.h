632747bab2d85f0e1282d7b129076a0e10488698
Stypox <stypox@pm.me>
2018-09-01 15:18:00 +0200

    Added app::input::MouseRoll class.
    It will generate events for mouse rolls.
    Removed app/input/mouse.* files because mouse rolls and mouse movements will be handled in two completely different classes, thus two sets of files.

--- into-space-remake/src/app/input/mouseroll.h
+++ into-space-remake/src/app/input/mouseroll.h
@@ -1,11 +1,13 @@
 #ifndef _INTO_SPACE_REMAKE_APP_INPUT_MOUSE_H_
 #define _INTO_SPACE_REMAKE_APP_INPUT_MOUSE_H_
 
-#include "../event/mouse.h"
+#include <GLFW/glfw3.h>
+
+#include "../event/mouseroll.h"
 #include "../event/handler.h"
 
 namespace app::input {
-	class Mouse {
+	class MouseRoll {
 
 	};
 }
