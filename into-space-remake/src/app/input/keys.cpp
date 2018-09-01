bf61721ecff7e312820b66b85346a8d0c5e9f296
Stypox <stypox@pm.me>
2018-09-01 14:31:42 +0200

    Added cursor position tracking to app::input::Keys.
    It can be activated or not for every type of event. app::input::Keys now supports 5 input event types (press, doublePress, release, longPress, longRelease), 2 types of position tracking (yes/no) and two key types (keyboard/mouse). Therefore there are 5*2*2=20 different variations that have to be processed differently. So to improve readability many comments have been added (e.g. press type, no position tracking, keyboard key).
    
    To give app::input::Keys::addListener() a better ordering the listener type is now deduced using a switch statement and an listener-id that is composed by or-ing event type, position tracking and key type.

--- into-space-remake/src/app/input/keys.cpp
+++ into-space-remake/src/app/input/keys.cpp
@@ -8,8 +8,8 @@ namespace app::input {
 
 
 	void Keys::updatePress() {
-		//keyboard
-		for (auto&& key : m_pressData) {
+		// press type, no position tracking, keyboard key
+		for (auto&& key : m_dataPress) {
 			if (bool keyState = glfwGetKey(m_window, key.key); keyState != key.lastState) {
 				if (keyState)
 					m_eventHandler.push(new event::Key{event::Key::press, key.key});
@@ -17,18 +17,36 @@ namespace app::input {
 			}
 		}
 
-		//mouse
-		for (auto&& key : m_mousePressData) {
+		// press type, no position tracking, mouse key
+		for (auto&& key : m_dataPressM) {
 			if (bool keyState = glfwGetMouseButton(m_window, key.key); keyState != key.lastState) {
 				if (keyState)
 					m_eventHandler.push(new event::Key{event::Key::press, key.key});
 				key.lastState = keyState;
 			}
 		}
+
+		// press type, position tracking, keyboard key
+		for (auto&& key : m_dataPressP) {
+			if (bool keyState = glfwGetKey(m_window, key.key); keyState != key.lastState) {
+				if (keyState)
+					m_eventHandler.push(new event::KeyPos{event::Key::press, key.key, m_xCursor, m_yCursor});
+				key.lastState = keyState;
+			}
+		}
+
+		// press type, position tracking, mouse key
+		for (auto&& key : m_dataPressPM) {
+			if (bool keyState = glfwGetMouseButton(m_window, key.key); keyState != key.lastState) {
+				if (keyState)
+					m_eventHandler.push(new event::KeyPos{event::Key::press, key.key, m_xCursor, m_yCursor});
+				key.lastState = keyState;
+			}
+		}
 	}
 	void Keys::updateDoublePress() {
-		//keyboard
-		for (auto&& key : m_doublePressData) {
+		// doublePress type, no position tracking, keyboard key
+		for (auto&& key : m_dataDoublePress) {
 			if (bool keyState = glfwGetKey(m_window, key.key); keyState != key.lastState) {
 				if (keyState) {
 					if (key.lastPressed + m_doublePressDelay > m_time) {
@@ -42,12 +60,42 @@ namespace app::input {
 			}
 		}
 
-		//mouse
-		for (auto&& key : m_mouseDoublePressData) {
+		// doublePress type, no position tracking, mouse key
+		for (auto&& key : m_dataDoublePressM) {
 			if (bool keyState = glfwGetMouseButton(m_window, key.key); keyState != key.lastState) {
 				if (keyState) {
 					if (key.lastPressed + m_doublePressDelay > m_time) {
 						m_eventHandler.push(new event::Key{event::Key::doublePress, key.key});
+						key.lastPressed = -3 * m_doublePressDelay;
+					}
+					else
+						key.lastPressed = m_time;
+				}
+				key.lastState = keyState;
+			}
+		}
+
+		// doublePress type, position tracking, keyboard key
+		for (auto&& key : m_dataDoublePressP) {
+			if (bool keyState = glfwGetKey(m_window, key.key); keyState != key.lastState) {
+				if (keyState) {
+					if (key.lastPressed + m_doublePressDelay > m_time) {
+						m_eventHandler.push(new event::KeyPos{event::Key::doublePress, key.key, m_xCursor, m_yCursor});
+						key.lastPressed = -3 * m_doublePressDelay;
+					}
+					else
+						key.lastPressed = m_time;
+				}
+				key.lastState = keyState;
+			}
+		}
+
+		// doublePress type, position tracking, mouse key
+		for (auto&& key : m_dataDoublePressPM) {
+			if (bool keyState = glfwGetMouseButton(m_window, key.key); keyState != key.lastState) {
+				if (keyState) {
+					if (key.lastPressed + m_doublePressDelay > m_time) {
+						m_eventHandler.push(new event::KeyPos{event::Key::doublePress, key.key, m_xCursor, m_yCursor});
 						key.lastPressed = -3 * m_doublePressDelay;
 					}
 					else
@@ -58,8 +106,8 @@ namespace app::input {
 		}
 	}
 	void Keys::updateRelease() {
-		//keyboard
-		for (auto&& key : m_releaseData) {
+		// release type, no position tracking, keyboard key
+		for (auto&& key : m_dataRelease) {
 			if (bool keyState = glfwGetKey(m_window, key.key); keyState != key.lastState) {
 				if (!keyState)
 					m_eventHandler.push(new event::Key{event::Key::release, key.key});
@@ -67,69 +115,135 @@ namespace app::input {
 			}
 		}
 
-		//mouse
-		for (auto&& key : m_mouseReleaseData) {
+		// release type, no position tracking, mouse key
+		for (auto&& key : m_dataReleaseM) {
 			if (bool keyState = glfwGetMouseButton(m_window, key.key); keyState != key.lastState) {
 				if (!keyState)
 					m_eventHandler.push(new event::Key{event::Key::release, key.key});
+				key.lastState = keyState;
+			}
+		}
+
+		// release type, position tracking, keyboard key
+		for (auto&& key : m_dataReleaseP) {
+			if (bool keyState = glfwGetKey(m_window, key.key); keyState != key.lastState) {
+				if (!keyState)
+					m_eventHandler.push(new event::KeyPos{event::Key::release, key.key, m_xCursor, m_yCursor});
+				key.lastState = keyState;
+			}
+		}
+
+		// release type, position tracking, mouse key
+		for (auto&& key : m_dataReleasePM) {
+			if (bool keyState = glfwGetMouseButton(m_window, key.key); keyState != key.lastState) {
+				if (!keyState)
+					m_eventHandler.push(new event::KeyPos{event::Key::release, key.key, m_xCursor, m_yCursor});
 				key.lastState = keyState;
 			}
 		}
 	}
 	void Keys::updateLongPress() {
-		//keyboard
-		for (auto&& key : m_longPressData) {
+		// longPress type, no position tracking, keyboard key
+		for (auto&& key : m_dataLongPress) {
 			if (bool keyState = glfwGetKey(m_window, key.key); keyState != key.lastState) {
 				key.lastState = keyState;
 				if (keyState)
 					key.lastAction = m_time;
 			}
-
 			if (key.lastState && key.lastAction + key.delayAfterAction < m_time) {
 				m_eventHandler.push(new event::KeyLong{event::KeyLong::press, key.key, key.delayAfterAction, key.delayInBetween});
 				key.lastAction += key.delayInBetween;
 			}
 		}
 
-		//mouse
-		for (auto&& key : m_mouseLongPressData) {
+		// longPress type, no position tracking, mouse key
+		for (auto&& key : m_dataLongPressM) {
 			if (bool keyState = glfwGetMouseButton(m_window, key.key); keyState != key.lastState) {
 				key.lastState = keyState;
 				if (keyState)
 					key.lastAction = m_time;
 			}
-
 			if (key.lastState && key.lastAction + key.delayAfterAction < m_time) {
 				m_eventHandler.push(new event::KeyLong{event::KeyLong::press, key.key, key.delayAfterAction, key.delayInBetween});
+				key.lastAction += key.delayInBetween;
+			}
+		}
+
+		// longPress type, position tracking, keyboard key
+		for (auto&& key : m_dataLongPressP) {
+			if (bool keyState = glfwGetKey(m_window, key.key); keyState != key.lastState) {
+				key.lastState = keyState;
+				if (keyState)
+					key.lastAction = m_time;
+			}
+			if (key.lastState && key.lastAction + key.delayAfterAction < m_time) {
+				m_eventHandler.push(new event::KeyPosLong{event::KeyLong::press, key.key, m_xCursor, m_yCursor, key.delayAfterAction, key.delayInBetween});
+				key.lastAction += key.delayInBetween;
+			}
+		}
+
+		// longPress type, position tracking, mouse key
+		for (auto&& key : m_dataLongPressPM) {
+			if (bool keyState = glfwGetMouseButton(m_window, key.key); keyState != key.lastState) {
+				key.lastState = keyState;
+				if (keyState)
+					key.lastAction = m_time;
+			}
+			if (key.lastState && key.lastAction + key.delayAfterAction < m_time) {
+				m_eventHandler.push(new event::KeyPosLong{event::KeyLong::press, key.key, m_xCursor, m_yCursor, key.delayAfterAction, key.delayInBetween});
 				key.lastAction += key.delayInBetween;
 			}
 		}
 	}
 	void Keys::updateLongRelease() {
-		//keyboard
-		for (auto&& key : m_longReleaseData) {
+		// longRelease type, no position tracking, keyboard key
+		for (auto&& key : m_dataLongRelease) {
 			if (bool keyState = glfwGetKey(m_window, key.key); keyState != key.lastState) {
 				key.lastState = keyState;
 				if (!keyState)
 					key.lastAction = m_time;
 			}
-
 			if (!key.lastState && key.lastAction + key.delayAfterAction < m_time) {
 				m_eventHandler.push(new event::KeyLong{event::KeyLong::release, key.key, key.delayAfterAction, key.delayInBetween});
 				key.lastAction += key.delayInBetween;
 			}
 		}
 
-		//mouse
-		for (auto&& key : m_mouseLongReleaseData) {
+		// longRelease type, no position tracking, mouse key
+		for (auto&& key : m_dataLongReleaseM) {
 			if (bool keyState = glfwGetMouseButton(m_window, key.key); keyState != key.lastState) {
 				key.lastState = keyState;
 				if (!keyState)
 					key.lastAction = m_time;
 			}
-
 			if (!key.lastState && key.lastAction + key.delayAfterAction < m_time) {
 				m_eventHandler.push(new event::KeyLong{event::KeyLong::release, key.key, key.delayAfterAction, key.delayInBetween});
+				key.lastAction += key.delayInBetween;
+			}
+		}
+
+		// longRelease type, position tracking, keyboard key
+		for (auto&& key : m_dataLongReleaseP) {
+			if (bool keyState = glfwGetKey(m_window, key.key); keyState != key.lastState) {
+				key.lastState = keyState;
+				if (!keyState)
+					key.lastAction = m_time;
+			}
+			if (!key.lastState && key.lastAction + key.delayAfterAction < m_time) {
+				m_eventHandler.push(new event::KeyPosLong{event::KeyLong::release, key.key, m_xCursor, m_yCursor, key.delayAfterAction, key.delayInBetween});
+				key.lastAction += key.delayInBetween;
+			}
+		}
+
+		// longRelease type, position tracking, mouse key
+		for (auto&& key : m_dataLongReleasePM) {
+			if (bool keyState = glfwGetMouseButton(m_window, key.key); keyState != key.lastState) {
+				key.lastState = keyState;
+				if (!keyState)
+					key.lastAction = m_time;
+			}
+			if (!key.lastState && key.lastAction + key.delayAfterAction < m_time) {
+				m_eventHandler.push(new event::KeyPosLong{event::KeyLong::release, key.key, m_xCursor, m_yCursor, key.delayAfterAction, key.delayInBetween});
 				key.lastAction += key.delayInBetween;
 			}
 		}
@@ -140,73 +254,96 @@ namespace app::input {
 		m_window{window}, m_eventHandler{eventHandler},
 		m_doublePressDelay{doublePressDelay} {}
 	Keys::Keys(GLFWwindow*& window, event::Handler& eventHandler, float doublePressDelay,
-		initializer_list<pair<event::Key::Type, int>> listeners,
-		initializer_list<tuple<event::KeyLong::Type, int, float, float>> longListeners) :
+		initializer_list<tuple<event::Key::Type, int, bool>> listeners,
+		initializer_list<tuple<event::KeyLong::Type, int, bool, float, float>> longListeners) :
 		m_window{window}, m_eventHandler{eventHandler},
 		m_doublePressDelay{doublePressDelay} {
 		for (auto&& listener : listeners)
-			addListener(get<event::Key::Type>(listener), get<int>(listener));
+			addListener(get<0>(listener), get<1>(listener), get<2>(listener));
 		for (auto&& listener : longListeners)
-			addListener(get<0>(listener), get<1>(listener), get<2>(listener), get<3>(listener));
+			addListener(get<0>(listener), get<1>(listener), get<2>(listener), get<3>(listener), get<4>(listener));
 	}
 
-	void Keys::addListener(event::Key::Type type, int key) {
-		//mouse
-		if (key >= 0 && key <= GLFW_MOUSE_BUTTON_LAST) {
-			switch (type) {
-				case event::Key::press:
-					m_mousePressData.emplace_back(key);
-					break;
-				case event::Key::doublePress:
-					m_mouseDoublePressData.emplace_back(key, m_doublePressDelay);
-					break;
-				case event::Key::release:
-					m_mouseReleaseData.emplace_back(key);
-					break;
-			}
-		}
-		//keyboard
-		else {
-			switch (type) {
-				case event::Key::press:
-					m_pressData.emplace_back(key);
-					break;
-				case event::Key::doublePress:
-					m_doublePressData.emplace_back(key, m_doublePressDelay);
-					break;
-				case event::Key::release:
-					m_releaseData.emplace_back(key);
-					break;
-			}
+	void Keys::addListener(event::Key::Type type, int key, bool trackPosition) {
+		bool isMouseKey = (key >= 0 && key <= GLFW_MOUSE_BUTTON_LAST);
+		int eventTypeId = (static_cast<int>(type) << 2) | (trackPosition << 1) | isMouseKey;
+		// bits are laid down this way ...0000ABCD where AB are the first two "type" bits, C is "trackPosition" and D is "isMouseKey"
+
+		switch (eventTypeId) {
+			case 0:		// press type, no position tracking, keyboard key
+				m_dataPress.emplace_back(key);
+				break;
+			case 1:		// press type, no position tracking, mouse key
+				m_dataPressM.emplace_back(key);
+				break;
+			case 2:		// press type, position tracking, keyboard key
+				m_dataPressP.emplace_back(key);
+				break;
+			case 3:		// press type, position tracking, mouse key
+				m_dataPressPM.emplace_back(key);
+				break;
+			case 4:		// doublePress type, no position tracking, keyboard key
+				m_dataDoublePress.emplace_back(key, m_doublePressDelay);
+				break;
+			case 5:		// doublePress type, no position tracking, mouse key
+				m_dataDoublePressM.emplace_back(key, m_doublePressDelay);
+				break;
+			case 6:		// doublePress type, position tracking, keyboard key
+				m_dataDoublePressP.emplace_back(key, m_doublePressDelay);
+				break;
+			case 7:		// doublePress type, position tracking, mouse key
+				m_dataDoublePressPM.emplace_back(key, m_doublePressDelay);
+				break;
+			case 8:		// release type, no position tracking, keyboard key
+				m_dataRelease.emplace_back(key);
+				break;
+			case 9:		// release type, no position tracking, mouse key
+				m_dataReleaseM.emplace_back(key);
+				break;
+			case 10:	// release type, position tracking, keyboard key
+				m_dataReleaseP.emplace_back(key);
+				break;
+			case 11:	// release type, position tracking, mouse key
+				m_dataReleasePM.emplace_back(key);
+				break;
 		}
 	}
-	void Keys::addListener(event::KeyLong::Type type, int key, float delayAfterAction, float delayInBetween) {
-		//mouse
-		if (key > 0 && key < GLFW_MOUSE_BUTTON_LAST) {
-			switch (type) {
-				case event::KeyLong::press:
-					m_mouseLongPressData.emplace_back(key, delayAfterAction, delayInBetween);
-					break;
-				case event::KeyLong::release:
-					m_mouseLongReleaseData.emplace_back(key, delayAfterAction, delayInBetween);
-					break;
-			}
-		}
-		//keyboard
-		else {
-			switch (type) {
-				case event::KeyLong::press:
-					m_longPressData.emplace_back(key, delayAfterAction, delayInBetween);
-					break;
-				case event::KeyLong::release:
-					m_longReleaseData.emplace_back(key, delayAfterAction, delayInBetween);
-					break;
-			}
+	void Keys::addListener(event::KeyLong::Type type, int key, bool trackPosition, float delayAfterAction, float delayInBetween) {
+		bool isMouseKey = (key >= 0 && key <= GLFW_MOUSE_BUTTON_LAST);
+		int eventTypeId = (static_cast<int>(type) << 2) | (trackPosition << 1) | isMouseKey;
+		// bits are laid down this way: ...0000ABC where A is the first "type" bit, B is "trackPosition" and D is "isMouseKey"
+
+		switch (eventTypeId) {
+			case 0:		// longPress type, no position tracking, keyboard key
+				m_dataLongPress.emplace_back(key, delayAfterAction, delayInBetween);
+				break;
+			case 1:		// longPress type, no position tracking, mouse key
+				m_dataLongPressM.emplace_back(key, delayAfterAction, delayInBetween);
+				break;
+			case 2:		// longPress type, position tracking, keyboard key
+				m_dataLongPressP.emplace_back(key, delayAfterAction, delayInBetween);
+				break;
+			case 3:		// longPress type, position tracking, mouse key
+				m_dataLongPressPM.emplace_back(key, delayAfterAction, delayInBetween);
+				break;
+			case 4:		// longRelease type, no position tracking, keyboard key
+				m_dataLongRelease.emplace_back(key, delayAfterAction, delayInBetween);
+				break;
+			case 5:		// longRelease type, no position tracking, mouse key
+				m_dataLongReleaseM.emplace_back(key, delayAfterAction, delayInBetween);
+				break;
+			case 6:		// longRelease type, position tracking, keyboard key
+				m_dataLongReleaseP.emplace_back(key, delayAfterAction, delayInBetween);
+				break;
+			case 7:		// longRelease type, position tracking, mouse key
+				m_dataLongReleasePM.emplace_back(key, delayAfterAction, delayInBetween);
+				break;
 		}
 	}
 
 	void Keys::update() {
 		m_time = static_cast<float>(glfwGetTime());
+		glfwGetCursorPos(m_window, &m_xCursor, &m_yCursor);
 		updatePress();
 		updateDoublePress();
 		updateRelease();
--- into-space-remake/src/app/input/keys.h
+++ into-space-remake/src/app/input/keys.h
@@ -2,7 +2,6 @@
 #define _INTO_SPACE_REMAKE_APP_INPUT_KEYS_H_
 
 #include <vector>
-#include <utility>
 #include <tuple>
 #include <GLFW/glfw3.h>
 
@@ -11,6 +10,8 @@
 
 namespace app::input {
 	class Keys {
+
+
 		struct KeyData {
 			const int key;
 			bool lastState;
@@ -43,19 +44,36 @@ namespace app::input {
 		event::Handler& m_eventHandler;
 		float m_doublePressDelay;
 
-		//listeners data
-		std::vector<KeyData> m_pressData;
-		std::vector<KeyData> m_mousePressData;
-		std::vector<KeyDoublePressData> m_doublePressData;
-		std::vector<KeyDoublePressData> m_mouseDoublePressData;
-		std::vector<KeyData> m_releaseData;
-		std::vector<KeyData> m_mouseReleaseData;
-		std::vector<KeyLongData> m_longPressData;
-		std::vector<KeyLongData> m_mouseLongPressData;
-		std::vector<KeyLongData> m_longReleaseData;
-		std::vector<KeyLongData> m_mouseLongReleaseData;
-		
+		//data of listeners (P -> track position, M -> mouse key)
+
+		//press
+		std::vector<KeyData> m_dataPress,						// press type, no position tracking, keyboard key
+							 m_dataPressM,						// press type, no position tracking, keyboard key
+							 m_dataPressP,						// press type, position tracking, keyboard key
+							 m_dataPressPM;						// press type, position tracking, mouse key
+		//doublePress
+		std::vector<KeyDoublePressData> m_dataDoublePress,		// doublePress type, no position tracking, keyboard key
+										m_dataDoublePressM,		// doublePress type, no position tracking, mouse key
+										m_dataDoublePressP,		// doublePress type, position tracking, keyboard key
+										m_dataDoublePressPM;	// doublePress type, position tracking, mouse key
+		//release
+		std::vector<KeyData> m_dataRelease,						// release type, no position tracking, keyboard key
+							 m_dataReleaseM,					// release type, no position tracking, mouse key
+							 m_dataReleaseP,					// release type, position tracking, keyboard key
+							 m_dataReleasePM;					// release type, position tracking, mouse key
+		//longPress
+		std::vector<KeyLongData> m_dataLongPress,				// longPress type, no position tracking, keyboard key
+								 m_dataLongPressM,				// longPress type, no position tracking, mouse key
+								 m_dataLongPressP,				// longPress type, position tracking, keyboard key
+								 m_dataLongPressPM;				// longPress type, position tracking, mouse key
+		//longRelease
+		std::vector<KeyLongData> m_dataLongRelease,				// longRelease type, no position tracking, keyboard key
+								 m_dataLongReleaseM,			// longRelease type, no position tracking, mouse key
+								 m_dataLongReleaseP,			// longRelease type, position tracking, keyboard key
+								 m_dataLongReleasePM;			// longRelease type, position tracking, mouse key
+
 		float m_time;
+		double m_xCursor, m_yCursor;
 
 		void updatePress();
 		void updateDoublePress();
@@ -65,18 +83,19 @@ namespace app::input {
 	public:
 		Keys(GLFWwindow*& window, event::Handler& eventHandler, float doublePressDelay);
 		Keys(GLFWwindow*& window, event::Handler& eventHandler, float doublePressDelay,
-			std::initializer_list<std::pair<event::Key::Type, int>> listeners,
-			std::initializer_list<std::tuple<event::KeyLong::Type, int, float, float>> longListeners);
+			std::initializer_list<std::tuple<event::Key::Type, int, bool>> listeners,
+			std::initializer_list<std::tuple<event::KeyLong::Type, int, bool, float, float>> longListeners);
 
 		inline void setDoublePressDelay(float doublePressDelay) { m_doublePressDelay = doublePressDelay; }
 
-		void addListener(event::Key::Type type, int key);
-		void addListener(event::KeyLong::Type type, int key, float delayAfterAction, float delayInBetween);
+		void addListener(event::Key::Type type, int key, bool trackPosition);
+		void addListener(event::KeyLong::Type type, int key, bool trackPosition, float delayAfterAction, float delayInBetween);
 
 		void update();
 
 		inline bool isPressed(int key) { return glfwGetKey(m_window, key); }
 	};
+
 }
 
 #endif
