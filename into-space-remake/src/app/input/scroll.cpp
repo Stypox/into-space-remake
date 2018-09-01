a0384625a726beed3405bb233b0bf02e0265c2bf
Stypox <stypox@pm.me>
2018-09-01 18:43:05 +0200

    Implemented app::input::Scroll class.
    It uses a static function (scrollCallback) that is called by glfw when the user scrolls (this is set using glfwSetScrollCallback). The pointer saved inside the GLFWwindow (http://www.glfw.org/docs/latest/window_guide.html#window_userptr) is assigned a std::vector<Scroll*> and everytime app::input::Scroll::scrollCallback() is called each Scroll in that vector is updated with the new scroll offsets by calling app::input::Scroll::updateOffset().
    Then when the user calls app::input::Scroll::update() all requested events are created.

--- into-space-remake/src/app/input/scroll.cpp
+++ into-space-remake/src/app/input/scroll.cpp
@@ -1,1 +1,75 @@
 #include "scroll.h"
+
+namespace app::input {
+	void Scroll::scrollCallback(GLFWwindow* window, double xOffset, double yOffset) {
+		if (void* userPointer = glfwGetWindowUserPointer(window); userPointer) {
+			std::vector<Scroll*>* scrollsToUpdate = static_cast<std::vector<Scroll*>*>(userPointer);
+			for (auto&& scroll : *scrollsToUpdate)
+				scroll->updateOffset(xOffset, yOffset);
+		}
+	}
+
+	void Scroll::updateOffset(double xOffset, double yOffset) {
+		m_xOffset += xOffset;
+		m_yOffset += yOffset;
+	}
+	
+	Scroll::Scroll(GLFWwindow*& window, event::Handler& eventHandler) :
+		Scroll{window, eventHandler, 0, 0} {}
+	Scroll::Scroll(GLFWwindow*& window, event::Handler& eventHandler, sp::Tint8 nrEventsX, sp::Tint8 nrEventsY) :
+		m_window{window}, m_eventHandler{eventHandler},
+		m_nrEventsX{nrEventsX}, m_nrEventsY{nrEventsY},
+		m_xOffset{0.0}, m_yOffset{0.0} {
+		activateWindowCallback();
+	}
+	Scroll::~Scroll() {
+		removeWindowCallback();
+	}
+
+	void Scroll::activateWindowCallback() {
+		if (m_window != nullptr) {
+			if (void* userPointer = glfwGetWindowUserPointer(m_window); userPointer) {
+				std::vector<Scroll*>* scrollsToUpdate = static_cast<std::vector<Scroll*>*>(userPointer);
+				if (std::find(scrollsToUpdate->begin(), scrollsToUpdate->end(), this) == scrollsToUpdate->end()) {
+					scrollsToUpdate->push_back(this);
+				}
+			}
+			else {
+				glfwSetWindowUserPointer(m_window, new std::vector<Scroll*> {this});
+				glfwSetScrollCallback(m_window, scrollCallback);
+			}
+		}
+	}
+	void Scroll::removeWindowCallback() {
+		if (m_window != nullptr) {
+			if (void* userPointer = glfwGetWindowUserPointer(m_window); userPointer) {
+				std::vector<Scroll*>* scrollsToUpdate = static_cast<std::vector<Scroll*>*>(userPointer);
+				if (auto thisScroll = std::find(scrollsToUpdate->begin(), scrollsToUpdate->end(), this); thisScroll != scrollsToUpdate->end()) {
+					scrollsToUpdate->erase(thisScroll, thisScroll + 1);
+				}
+				if (scrollsToUpdate->size() == 0) {
+					glfwSetScrollCallback(m_window, nullptr);
+					glfwSetWindowUserPointer(m_window, nullptr);
+					delete scrollsToUpdate;
+				}
+			}
+		}
+	}
+
+	void Scroll::update() {
+		double xCursor, yCursor;
+		glfwGetCursorPos(m_window, &xCursor, &yCursor);
+
+		if (m_xOffset != 0) {
+			for (sp::Tint8 xEvents = 0; xEvents != m_nrEventsX; ++xEvents)
+				m_eventHandler.push(new event::Scroll{event::Scroll::xAxis, m_xOffset, xCursor, yCursor});
+			m_xOffset = 0;
+		}
+
+		if (m_yOffset != 0) {
+			for (sp::Tint8 yEvents = 0; yEvents != m_nrEventsY; ++yEvents)
+				m_eventHandler.push(new event::Scroll{event::Scroll::yAxis, m_yOffset, xCursor, yCursor});
+			m_yOffset = 0;
+		}
+	}
+}
--- into-space-remake/src/app/input/scroll.h
+++ into-space-remake/src/app/input/scroll.h
@@ -1,14 +1,37 @@
 #ifndef _INTO_SPACE_REMAKE_APP_INPUT_MOUSE_H_
 #define _INTO_SPACE_REMAKE_APP_INPUT_MOUSE_H_
 
+#include <vector>
+#include <algorithm>
 #include <GLFW/glfw3.h>
+#include <stypox/basicTypes.h>
 
 #include "../event/scroll.h"
 #include "../event/handler.h"
 
 namespace app::input {
 	class Scroll {
+		static void scrollCallback(GLFWwindow* window, double xOffset, double yOffset);
 
+		GLFWwindow*& m_window;
+		event::Handler& m_eventHandler;
+		sp::Tint8 m_nrEventsX, m_nrEventsY;
+
+		double m_xOffset, m_yOffset;
+	
+		void updateOffset(double xOffset, double yOffset);
+	public:
+		Scroll(GLFWwindow*& window, event::Handler& eventHandler);
+		Scroll(GLFWwindow*& window, event::Handler& eventHandler, sp::Tint8 nrEventsX, sp::Tint8 nrEventsY);
+		~Scroll();
+
+		inline void setNrEventsX(sp::Tint8 nrEventsX) { m_nrEventsX = nrEventsX; }
+		inline void setNrEventsY(sp::Tint8 nrEventsY) { m_nrEventsY = nrEventsY; }
+
+		void activateWindowCallback();
+		void removeWindowCallback();
+
+		void update();
 	};
 }
 
