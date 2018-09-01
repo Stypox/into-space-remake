ef4f1253f4969784a82b35ece3f097dc397db0e1
Stypox <stypox@pm.me>
2018-09-01 18:13:02 +0200

    Modified app::event::Scroll.
    Added a app::event::Scroll::type member that tells whether the Event represents a Scroll along the X or along the Y axis.

--- into-space-remake/src/app/event/scroll.h
+++ into-space-remake/src/app/event/scroll.h
@@ -5,12 +5,19 @@
 
 namespace app::event {
 	struct Scroll : public Event {
-		const double offset;
-		const double x, y;
+		enum Type {
+			xAxis,
+			yAxis,
+		};
 
-		constexpr Scroll(double offset, double x, double y) :
-			Event{Event::Type::mouseRoll}, offset{offset},
-			x{x}, y{y} {}
+		const Type type;
+		const double offset;
+		const double xCursor, yCursor;
+
+		constexpr Scroll(Type type, double offset, double xCursor, double yCursor) :
+			Event{Event::Type::mouseRoll}, type{type},
+			offset{offset}, xCursor{xCursor},
+			yCursor{yCursor} {}
 
 		inline Event::Type eventType() const override { return Event::type; }
 		inline operator bool() const override { return Event::type != Event::Type::empty; }
