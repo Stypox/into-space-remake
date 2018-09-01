c5dc41f56f7ddf3849fb2ce277361874475b1cdb
Stypox <stypox@pm.me>
2018-09-01 14:18:26 +0200

    Removed now useless Click and LongClick event types.
    They were supposed to keep track of the position of the cursor, but that functionality has been replaced by KeyPos and KeyPosLong.

--- into-space-remake/src/app/event/mouse.h
+++ into-space-remake/src/app/event/mouse.h
@@ -4,52 +4,11 @@
 #include "event.h"
 
 namespace app::event {
-	struct Click : public Event {
-		enum Type : char {
-			press,
-			doublePress,
-			release,
-		};
-
-		const Type type;
-		const int key;
-		const int x, y;
-
-		constexpr Click(Type type, int key, int x, int y) :
-			Event{Event::Type::click}, type{type},
-			key{key}, x{x},
-			y{y} {}
-
-		inline Event::Type eventType() const override { return Event::type; }
-		inline operator bool() const override { return Event::type != Event::Type::empty; }
-	};
-
-	struct LongClick : public Event {
-		enum Type : char {
-			press,
-			release,
-		};
-
-		const Type type;
-		const int key;
-		const int x, y;
-		const float delayAfterAction, delayInBetween;
-
-		constexpr LongClick(Type type, int key, int x, int y, float delayAfterAction, float delayInBetween) :
-			Event{Event::Type::click}, type{type},
-			key{key}, x{x},
-			y{y}, delayAfterAction{delayAfterAction},
-			delayInBetween{delayInBetween} {}
-
-		inline Event::Type eventType() const override { return Event::type; }
-		inline operator bool() const override { return Event::type != Event::Type::empty; }
-	};
-
 	struct MouseRoll : public Event {
 		const int offset;
-		const int x, y;
+		const double x, y;
 
-		constexpr MouseRoll(int offset, int x, int y) :
+		constexpr MouseRoll(int offset, double x, double y) :
 			Event{Event::Type::mouseRoll}, offset{offset},
 			x{x}, y{y} {}
 
