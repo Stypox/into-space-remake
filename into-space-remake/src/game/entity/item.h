d54b1d6c1fe695f9959c730c7a4282912fa0f3cd
Stypox <stypox@pm.me>
2018-09-04 18:28:35 +0200

    Added game::entity::Item interface

---
+++ into-space-remake/src/game/entity/item.h
@@ -0,0 +1,30 @@
+#ifndef _INTO_SPACE_REMAKE_GAME_ENTITY_ITEM_H_
+#define _INTO_SPACE_REMAKE_GAME_ENTITY_ITEM_H_
+
+#include "entity.h"
+
+namespace game::entity {
+	constexpr size_t nrVerticesItem = 4;
+
+	class Item : public Entity<nrVerticesItem> {
+		enum Type : char { //the enum values represent the position in the texture file 
+			fuel0  = 0,
+			fuel1  = 1,
+			fuel2  = 2,
+			money0 = 3,
+			money1 = 4,
+			money2 = 5,
+			repair = 6,
+		};
+
+		const Type m_type;
+		const float m_x, m_y;
+
+	public:
+		Item(float x, float y);
+
+		std::array<float, nrVerticesItem * render::vertexSize> vertices();
+	};
+}
+
+#endif
