1531eabb454a3898bb5fae52fe60ea21993b28dc
Stypox <stypox@pm.me>
2018-09-04 18:20:15 +0200

    Fixed mistypings in game/entity/entity.h

--- into-space-remake/src/game/entity/entity.h
+++ into-space-remake/src/game/entity/entity.h
@@ -1,11 +1,11 @@
-#ifndef _INTO_SPACE_REMAKE_GAME_ENTITY_H_
-#define _INTO_SPACE_REMAKE_GAME_ENTITY_H_
+#ifndef _INTO_SPACE_REMAKE_GAME_ENTITY_ENTITY_H_
+#define _INTO_SPACE_REMAKE_GAME_ENTITY_ENTITY_H_
 
 #include <array>
 #include <glm/glm.hpp>
 
 namespace game::entity {
-	template<typename V>
+	template<size_t V>
 	class Entity {		
 	public:
 		virtual std::array<float, V> vertices() = 0;
