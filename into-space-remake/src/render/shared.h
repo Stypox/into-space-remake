ac8d15af6e5afa65764bd4a566a49bf50920df26
Stypox <stypox@pm.me>
2018-09-07 21:19:11 +0200

    src/render/shared.h now includes shaderDir and itemTexturePos constants.

--- into-space-remake/src/render/shared.h
+++ into-space-remake/src/render/shared.h
@@ -2,10 +2,15 @@
 #define _INTO_SPACE_REMAKE_RENDER_SHARED_H_
 
 #include <cstddef>
+#include <filesystem>
+#include <basicTypes.h>
+#include <GLFW/glfw3.h>
 
 namespace render {
 	constexpr size_t vertexSize = 4; // x, y, u, v
-	
+	const sp::Tpath shaderDir = "./into-space-remake/src/render/shader/";
+
+	constexpr GLint itemTexturePos = 0;
 }
 
 #endif
