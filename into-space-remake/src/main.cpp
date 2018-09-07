17e22252b6041ed21a8e315c3c979e8edda613c0
Stypox <stypox@pm.me>
2018-09-07 21:10:26 +0200

    Set texture directory in src/main.cpp.
    Also added some comments to main.cpp to make understanding simpler.

--- into-space-remake/src/main.cpp
+++ into-space-remake/src/main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 #include <range.h>
+#include <gl-abstractions/texture.h>
 
 #include "app/application.h"
 
 int main(int argc, char* argv[]) {
+	//put arguments in vector
 	std::vector<std::string> arguments;
 	for (int arg : sp::range(0, argc))
 		arguments.push_back(argv[arg]);
 
+	//print start label
 	std::string lines(6 + arguments[0].length(), '-');
 	std::cout << lines << "\nStart " << arguments[0] << "\n" << lines << "\n";
 
+	//settings
+	sp::gl::Texture2D::setDirectory("./into-space-remake/res/");
+
+	//run and catch exceptions
 	int exitValue;
 	try {
 		app::Arguments::parse(arguments);
@@ -25,6 +32,7 @@ int main(int argc, char* argv[]) {
 		exitValue = 2;
 	}
 
+	//print end label
 	std::cout << lines << "\n End " << arguments[0] << " \n" << lines << "\n";
 	return exitValue;
 }
