53c8ece9423708b3dcf553a8e35ae12c8ac13b40
Stypox <stypox@pm.me>
2018-09-11 14:14:50 +0200

    Added constructor to game::entity::movable::Movable.
    This removes undefined behaviour.

--- into-space-remake/src/game/entity/movable/movable.cpp
+++ into-space-remake/src/game/entity/movable/movable.cpp
@@ -1,6 +1,9 @@
 #include "movable.h"
 
 namespace game::entity::movable {
+	Movable::Movable(GLfloat x, GLfloat y) :
+		m_x{x}, m_y{y} {}
+	
 	GLfloat Movable::x() const {
 		return m_x;
 	}
--- into-space-remake/src/game/entity/movable/movable.h
+++ into-space-remake/src/game/entity/movable/movable.h
@@ -9,6 +9,8 @@ namespace game::entity::movable {
 	class Movable : public Entity {
 		GLfloat m_x, m_y;
 	public:
+		Movable(GLfloat x, GLfloat y);
+
 		GLfloat x() const override;
 		GLfloat y() const override;
 		virtual GLfloat sizeX() const = 0;
