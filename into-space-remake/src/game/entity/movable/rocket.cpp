02724b0db9e81d8e939d78d8936cce32810d7030
Stypox <stypox@pm.me>
2018-09-17 15:55:08 +0200

    Added game::entity::movable::Rocket::pickUpIntersecting()
    Picks up items that collide with the rocket, removing them from the list.

--- into-space-remake/src/game/entity/movable/rocket.cpp
+++ into-space-remake/src/game/entity/movable/rocket.cpp
@@ -1,6 +1,43 @@
 #include "rocket.h"
 
+#include <iostream>
+
 namespace game::entity::movable {
+	void Rocket::pickUp(const Item& item) {
+		switch (item.type()) {
+		case Item::fuel0:
+			std::cout << "fuel 0\n";
+			break;
+		case Item::fuel1:
+			std::cout << "fuel 1\n";
+			break;
+		case Item::fuel2:
+			std::cout << "fuel 2\n";
+			break;
+		case Item::money0:
+			std::cout << "money 0\n";
+			break;
+		case Item::money1:
+			std::cout << "money 1\n";
+			break;
+		case Item::money2:
+			std::cout << "money 2\n";
+			break;
+		case Item::repair:
+			std::cout << "repair\n";
+			break;
+		}
+	}
+
 	Rocket::Rocket() :
 		Movable{0.0, 0.0} {}
+
+	void Rocket::pickUpIntersecting(std::vector<std::unique_ptr<entity::Item>>& items) {
+		for (auto item = items.end(); item != items.begin(); --item) {
+			if (intersects(this, item->get())) {
+				pickUp(**item);
+				items.erase(item, item + 1);
+			}
+		}
+	}
 }
--- into-space-remake/src/game/entity/movable/rocket.h
+++ into-space-remake/src/game/entity/movable/rocket.h
@@ -2,11 +2,16 @@
 #define _INTO_SPACE_REMAKE_GAME_ENTITY_MOVABLE_ROCKET_H_
 
 #include <GLFW/glfw3.h>
+#include <vector>
+#include <memory>
 
 #include "movable.h"
+#include "../item.h"
 
 namespace game::entity::movable {
 	class Rocket : public Movable {
+		void pickUp(const Item& item);
+
 	public:
 		Rocket();
 
@@ -14,6 +19,8 @@ namespace game::entity::movable {
 		GLfloat sizeY() const override { return 0.2f; }
 		GLfloat textureOffset() const override { return 0.0f; }
 		GLfloat textureWidth() const override { return 1.0f; }
+
+		void pickUpIntersecting(std::vector<std::unique_ptr<Item>>& items);
 	};
 }
 
