47121b5416acfeb1824e6975b4ca99aeec58b13e
Stypox <stypox@pm.me>
2018-12-26 10:40:24 +0100

    Added lastTimeRanInto attribute to cloud.
    It is used by the rocket to check how much time has passed since the last time the cloud slowed it down.

--- into-space-remake/src/game/ent/mov/cloud.cpp
+++ into-space-remake/src/game/ent/mov/cloud.cpp
@@ -3,9 +3,9 @@
 #include "../../../misc/random.h"
 
 namespace game::ent::mov {
-	Cloud::Cloud(GLfloat x, GLfloat y) :
-		Movable{RenderData{x, y, 0.0f, 0.0f, 0.0f, 0.2f, 0.8f}} {
-		GLfloat size = misc::Random::range(0.3f, 0.4f);
+	Cloud::Cloud(float x, float y) :
+		Movable{RenderData{x, y, 0.0f, 0.0f, 0.0f, 0.2f, 0.8f}}, lastTimeRanInto{0.0f} {
+		float size = misc::Random::range(0.3f, 0.4f);
 		m_renderData->h = size;
 		m_renderData->w = size * widthHeightRatio;
 		m_renderData->rotation = misc::Random::range(-0.3f, 0.3f);
--- into-space-remake/src/game/ent/mov/cloud.h
+++ into-space-remake/src/game/ent/mov/cloud.h
@@ -1,15 +1,17 @@
 #ifndef _INTO_SPACE_REMAKE_GAME_ENT_MOV_CLOUD_H_
 #define _INTO_SPACE_REMAKE_GAME_ENT_MOV_CLOUD_H_
 
-#include <glad/glad.h>
-
 #include "movable.h"
 
 namespace game::ent::mov {
 	class Cloud : public Movable {
-		static constexpr GLfloat widthHeightRatio = 1.5f;
+		friend class Rocket;
+
+		static constexpr float widthHeightRatio = 1.5f;
+
+		float lastTimeRanInto;
 	public:
-		Cloud(GLfloat x, GLfloat y);
+		Cloud(float x, float y);
 
 		void updatePosition(float deltaTime) override {};
 	};
